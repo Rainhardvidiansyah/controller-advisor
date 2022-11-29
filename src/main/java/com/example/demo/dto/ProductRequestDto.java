26ef2b81d922176f165c18e534102ea2f479accb
Rainhardvidiansyah <rainhardvidians@gmail.com>
2022-11-29 15:49:14 +0700

    add field available in product request dto

--- src/main/java/com/example/demo/dto/ProductRequestDto.java
+++ src/main/java/com/example/demo/dto/ProductRequestDto.java
@@ -2,6 +2,7 @@ package com.example.demo.dto;
 
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,9 +13,13 @@ import lombok.Setter;
 @AllArgsConstructor @NoArgsConstructor
 public class ProductRequestDto {
 
+    @NotEmpty
     @NotBlank(message = "Product name must be written")
     private String product_name;
 
     @NotNull(message = "Product price must be written")
     private int price;
+
+    @NotEmpty @NotBlank
+    private boolean available;
 }
