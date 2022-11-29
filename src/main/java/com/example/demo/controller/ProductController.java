6e1bbe6349bd93ceb34d661d89edae89289befc8
Rainhardvidiansyah <rainhardvidians@gmail.com>
2022-11-29 15:57:57 +0700

    add its dependent to productservice

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -10,9 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.UUID;
-
 @RestController
 @RequestMapping("/api/v1/product")
 @RequiredArgsConstructor
@@ -20,26 +17,19 @@ public class ProductController {
 
     private final ProductService productService;
 
-    private static List<Product> products(){
-        return List.of(
-                new Product(UUID.randomUUID().toString(), "Product Satu", 2000, true),
-                new Product(UUID.randomUUID().toString(), "Product Dua", 23000, true),
-                new Product(UUID.randomUUID().toString(), "Product Tiga", 10000, false)
-        );
-    }
-
-    @GetMapping("/")
-    public ResponseEntity<?> getAllProduct(){
-        List<Product> products = products();
-        return new ResponseEntity<>(products, HttpStatus.OK);
-    }
-
     @PostMapping("/save-data")
     public ResponseEntity<?> saveProduct(@RequestBody @Valid ProductRequestDto productDto){
-        Product product = productService.saveProduct(Product.saveFrom(productDto));
+        Product product = productService.createProduct(Product.saveFrom(productDto));
         return new ResponseEntity<>("SUCCESS", HttpStatus.OK);
     }
 
+
+
+
+}
+
+
+/*
     @GetMapping("/data")
     @ResponseBody
     public ResponseEntity<?> fizz(@RequestParam String point){
@@ -53,7 +43,4 @@ public class ProductController {
     private static boolean validatePoint(String point){
         return !point.equalsIgnoreCase("fizz");
     }
-
-
-
-}
+ */
