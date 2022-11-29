e3f13b210674d447c74566d4db6d7d7a860bed02
Rainhardvidiansyah <rainhardvidians@gmail.com>
2022-11-29 15:47:14 +0700

    extends DAO to repository

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -1,17 +1,11 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
-import java.util.List;
-import java.util.Optional;
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
-public interface ProductRepository {
-
-    public Product saveProduct(Product product);
-
-    public List<Product> findAllProduct();
-
-    public Optional<Product> findProductName(String productName);
 
 }
