f686dad01f5e464407462ee822b0d3dfa27bd1d7
Rainhardvidiansyah <rainhardvidians@gmail.com>
2022-11-29 19:03:35 +0700

    Refactor ErrorDetails class, add status field

--- src/main/java/com/example/demo/response/ErrorDetails.java
+++ src/main/java/com/example/demo/response/ErrorDetails.java
@@ -6,17 +6,24 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@NoArgsConstructor @Getter @Setter
+@Getter @Setter
 public class ErrorDetails {
 
     private LocalDateTime date;
     private String message;
+
+    private int status;
     private String details;
 
-    public ErrorDetails(LocalDateTime date, String message, String details) {
+    public ErrorDetails(){
         super();
-        this.date = date;
+        this.date = LocalDateTime.now();
+    }
+
+    public ErrorDetails(LocalDateTime date, String message, int status, String details) {
+        super();
         this.message = message;
+        this.status = status;
         this.details = details;
     }
 }
